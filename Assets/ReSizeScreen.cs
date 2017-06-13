86e92db48f2deee72da9258b6c852d251e59fafd
Miao <uno.niceday@gmail.com>
2017-06-13 21:37:38 +0800

    uno
    ResizeScren
    調整螢幕自適應大小
    ScreenSprite
    貼滿整個螢幕的sprite

---
+++ Assets/ReSizeScreen.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReSizeScreen : MonoBehaviour {
+
+
+    public float baseWidth = 1024;
+    public float baseHeight = 768;
+    public float baseOrthographicSize = 5;
+
+    void Awake()
+    {
+        float newOrthographicSize = (float)Screen.height / (float)Screen.width * this.baseWidth / this.baseHeight * this.baseOrthographicSize;
+        Camera.main.orthographicSize = Mathf.Max(newOrthographicSize, this.baseOrthographicSize);
+
+    }
+
+  
+}
---
+++ Assets/ScreenSprite.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenSprite : MonoBehaviour {
+
+    
+    private void Awake()
+    {
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+
+        //攝影機垂直單位*2 = 全高，默認是5，因此這裡=10
+        //假設螢幕是1920x1080 ， 螢幕上每格的單位就會是108pixel (1080/10)
+        //然後寬度就會是17.7格 
+        float cameraHeight = Camera.main.orthographicSize * 2;
+        Vector2 cameraSize = new Vector2(Camera.main.aspect * cameraHeight, cameraHeight);
+        Vector2 spriteSize = spriteRenderer.sprite.bounds.size;
+
+        //算出寬高去選取縮放哪一邊
+        Vector2 scale = transform.localScale;
+        if (cameraSize.x >= cameraSize.y)
+        { 
+            scale *= cameraSize.x / spriteSize.x;
+        }
+        else
+        { 
+            scale *= cameraSize.y / spriteSize.y;
+        }
+
+        //將圖片置中，並改變為設置的大小
+        transform.position = Vector2.zero; 
+        transform.localScale = scale;
+    }
+   
+}
